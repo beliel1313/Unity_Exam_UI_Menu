7ea3f161c6ee3cbcd237363c9aa1abaae83985c1
beliel1313 <55227114+beliel1313@users.noreply.github.com>
2019-11-07 14:43:31 +0800

    E1080330 王人霈

---
+++ UnityProject/Assets/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    public Text loadingText;
+    public Slider loadingBar;
+
+    public void BGM_vol (float value)
+    {
+        mixer.SetFloat("VBGM", value);
+    }
+
+    public void SFX_vol(float value)
+    {
+        mixer.SetFloat("VSFX", value);
+    }
+
+    // Use this for initialization
+    void Start ()
+    {
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
